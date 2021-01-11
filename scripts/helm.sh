33a183a1f10c570de09eebe543f972b38504e916
Luke Addison <lukeaddison785@gmail.com>
2021-01-11 13:37:58 +0000

    Add Jenkins values

--- scripts/helm.sh
+++ scripts/helm.sh
@@ -4,7 +4,11 @@ set -euxo pipefail
 
 STAGING_DIR=$1
 
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
 helm repo update
 
 helm template charts/nginx --output-dir $STAGING_DIR
-helm template jenkins jenkins/jenkins --namespace jenkins --output-dir $STAGING_DIR
+helm template jenkins jenkins/jenkins --namespace jenkins \
+  -f $DIR/../charts/jenkins/values.yaml \
+  --output-dir $STAGING_DIR
