b89d58d94bf5afa62cd1cebff69911dd483684ee
Luke Addison <lukeaddison785@gmail.com>
2021-02-14 22:12:07 +0000

    Remove nginx

--- scripts/helm.sh
+++ scripts/helm.sh
@@ -8,7 +8,6 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 helm repo update
 
-helm template charts/nginx --output-dir $STAGING_DIR
 # https://github.com/jenkinsci/helm-charts
 helm template jenkins jenkins/jenkins --namespace jenkins \
   --version $JENKINS_VERSION \
